cea64add202ec42d9f6aa5588bbc64beacaf8756
KryvavyiPotii <144026226+KryvavyiPotii@users.noreply.github.com>
2024-05-21 23:16:09 +0200

    Create statistics.h
    - Contains T- and F-distribution with significance level 5%

---
+++ statistics.h
@@ -0,0 +1,39 @@
+// Student's t-distribution critical values for significance level 0.05.
+// Degrees of freedom from 1 to 15.
+double t_05[] = {
+    12.706,  // df = 1
+    4.303,   // df = 2
+    3.182,   // df = 3
+    2.776,   // df = 4
+    2.571,   // df = 5
+    2.447,   // df = 6
+    2.365,   // df = 7
+    2.306,   // df = 8
+    2.262,   // df = 9
+    2.228,   // df = 10
+    2.201,   // df = 11
+    2.179,   // df = 12
+    2.160,   // df = 13
+    2.145,   // df = 14
+    2.131    // df = 15
+};
+
+// F-distribution for significance level 0.05.
+// Both degrees of freedom from 1 to 15
+double f_05[] = {
+    161.448, // df1 = df2 = 1
+    19.000,  // df1 = df2 = 2
+    9.277,   // df1 = df2 = 3
+    6.388,   // df1 = df2 = 4
+    5.050,   // df1 = df2 = 5
+    4.284,   // df1 = df2 = 6
+    3.787,   // df1 = df2 = 7
+    3.438,   // df1 = df2 = 8
+    3.179,   // df1 = df2 = 9
+    2.978,   // df1 = df2 = 10
+    2.818,   // df1 = df2 = 11
+    2.687,   // df1 = df2 = 12
+    2.577,   // df1 = df2 = 13
+    2.484,   // df1 = df2 = 14
+    2.403    // df1 = df2 = 15
+};
